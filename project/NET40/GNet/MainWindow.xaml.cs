22b78267085a62920f477779030fe74d59284003
jabb <jj.jabbaloo@gmail.com>
2011-04-13 11:01:56 -0600

    reworked project directories; there are now build items for .NET 2.0, .NET 3.5, and .NET 4.0

---
+++ project/NET40/GNet/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GNet
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetupCommandBindings();
+
+        }
+
+        private void SetupCommandBindings()
+        {
+            var binding = new CommandBinding(ApplicationCommands.New);
+            binding.Executed += (s, e) =>
+            {
+
+            };
+            binding.CanExecute += (s, e) =>
+            {
+                e.CanExecute = true;
+            };
+            CommandBindings.Add(binding);
+        }
+    }
+}
