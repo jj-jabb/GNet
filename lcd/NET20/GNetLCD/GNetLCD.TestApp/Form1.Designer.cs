871f1dec657fa5706a0640afec9f7a2b2d6e2b09
jabb <jj.jabbaloo@gmail.com>
2011-04-16 21:26:07 -0600

    can now write to the lcd

--- lcd/NET20/GNetLCD/GNetLCD.TestApp/Form1.Designer.cs
+++ lcd/NET20/GNetLCD/GNetLCD.TestApp/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.btnConnect = new System.Windows.Forms.Button();
             this.btnDisconnect = new System.Windows.Forms.Button();
+            this.btnWrite = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnConnect
@@ -44,7 +45,7 @@
             // 
             // btnDisconnect
             // 
-            this.btnDisconnect.Location = new System.Drawing.Point(12, 41);
+            this.btnDisconnect.Location = new System.Drawing.Point(12, 70);
             this.btnDisconnect.Name = "btnDisconnect";
             this.btnDisconnect.Size = new System.Drawing.Size(75, 23);
             this.btnDisconnect.TabIndex = 1;
@@ -52,11 +53,22 @@
             this.btnDisconnect.UseVisualStyleBackColor = true;
             this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
             // 
+            // btnWrite
+            // 
+            this.btnWrite.Location = new System.Drawing.Point(12, 41);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(75, 23);
+            this.btnWrite.TabIndex = 2;
+            this.btnWrite.Text = "Write";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(646, 458);
+            this.Controls.Add(this.btnWrite);
             this.Controls.Add(this.btnDisconnect);
             this.Controls.Add(this.btnConnect);
             this.Name = "Form1";
@@ -69,6 +81,7 @@
 
         private System.Windows.Forms.Button btnConnect;
         private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.Button btnWrite;
     }
 }
 
--- lcd/NET20/GNetLCD/GNetLCD.TestApp/Form1.cs
+++ lcd/NET20/GNetLCD/GNetLCD.TestApp/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Text;
 using System.Windows.Forms;
 
@@ -17,22 +18,67 @@ namespace GNetLCD.TestApp
         public Form1()
         {
             InitializeComponent();
-
-
         }
 
         private void btnConnect_Click(object sender, EventArgs e)
         {
             lcd = new Lcd("GNet Test", true, false, LcdDeviceType.LcdDeviceBW);
+            lcd.Connect();
+            lcd.Open();
             lcd.BringToFront();
+        }
+
+        //http://www.vbforums.com/showthread.php?t=358917
+        // import this 
+        // using System.Runtime.InteropServices;
+        //private unsafe byte[] BmpToBytes_Unsafe(Bitmap bmp)
+        //{
+        //    BitmapData bData = bmp.LockBits(new Rectangle(new Point(), bmp.Size),
+        //        ImageLockMode.ReadOnly,
+        //        PixelFormat.Format24bppRgb);
+        //    // number of bytes in the bitmap
+        //    int byteCount = bData.Stride * bmp.Height;
+        //    byte[] bmpBytes = new byte[byteCount];
+
+        //    // Copy the locked bytes from memory
+        //    Marshal.Copy(bData.Scan0, bmpBytes, 0, byteCount);
+
+        //    // don't forget to unlock the bitmap!!
+        //    bmp.UnlockBits(bData);
+
+        //    return bmpBytes;
+        //}
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            Bitmap bm = new Bitmap(160, 43, PixelFormat.Format1bppIndexed);
+
+            for (int i = 0; i < 160; i++)
+                for (int j = 0; j < 43; j++)
+                    bm.SetPixel(i, j, Color.White);
+
+            bm.GetHbitmap()
+
+            byte[] b = new byte[Lcd.BW_SIZE];
+
+            for (int i = 0; i < b.Length; i++)
+            {
+                if (i % 2 == 0)
+                    b[i] = 255;
+            }
+
+            lcd.UpdateBitmap(b, LcdPriority.Normal);
         }
 
         private void btnDisconnect_Click(object sender, EventArgs e)
         {
             if (lcd != null)
             {
+                //lcd.Close();
+                //lcd.Disconnect();
+
                 lcd.Dispose();
-                lcd = null;
+                //lcd = null;
             }
         }
     }
