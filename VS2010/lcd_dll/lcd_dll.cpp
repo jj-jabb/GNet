9b3d2cc8480093f878525618b9b922bc2976379e
jabb <jj.jabbaloo@gmail.com>
2011-05-13 12:26:22 -0600

    cleanup for refactor

--- VS2010/lcd_dll/lcd_dll.cpp
+++
@@ -1,5 +0,0 @@
-// lcd_dll.cpp : Defines the entry point for the DLL application.
-//
-
-#include "stdafx.h"
-// This file is needed just to avoid a link error LNK2001 
--- VS2010/src/Profiles/_template.lua
+++
@@ -1,5 +0,0 @@
-﻿-- NOTE: currently family is ALWAYS 'lhc' since the only currently supported
--- device is the G13 left-handed-controller.
-function OnEvent(event, arg, family)
-	
-end
